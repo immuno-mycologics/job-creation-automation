5d51bc8af20ee280bd3175a6a2b504e262fc6d4e
sreenath-umagandhi <42354674+sreenath-umagandhi@users.noreply.github.com>
2024-01-18 08:28:07 -0500

    working version

--- app.js
+++ app.js
@@ -44,6 +44,60 @@ app.post('/api/createDirectory', async (req, res) => {
 
 })
 
+app.post('/api/uploadReport', async (req, res) => {
+
+    console.log(req.body)
+    let Job = req.body.Job.replace(/\s/g,'')
+    let itemLotJob = req.body.Item+' - '+req.body.Lot+' - '+Job;
+    let item = req.body.Item;
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    };
+
+    axios.post('https://accounts.accesscontrol.windows.net/f1f1b8d0-6c95-49c7-bea5-ad1ad4acace6/tokens/OAuth/2',{
+        grant_type: 'client_credentials',
+        client_id: '187a1f2d-88d7-422c-bf8d-51f94c9b4d79@f1f1b8d0-6c95-49c7-bea5-ad1ad4acace6',
+        client_secret: 'm9Q/ZEsFrKDGXe7L3ZKe2rooWhyj+2rcT/w34AQbhpY=',
+        resource: '00000003-0000-0ff1-ce00-000000000000/immy2700.sharepoint.com@f1f1b8d0-6c95-49c7-bea5-ad1ad4acace6'
+    },config)
+        .then(function (response) {
+            console.log(response.data);
+            let accessToken = response.data.access_token;
+            if (accessToken){
+                let config1 = {
+                    headers: {
+                        'Accept': 'application/json;odata=nometadata',
+                        'Authorization': 'Bearer '+accessToken,
+                        'X-RequestDigest': '0x703AE026F5E314B4F898AE91F77BDA1D192553AD50585C5018FCE8BD69BA6DC71EA26D88A4E650235663C76804D9048B2E7D1FB786702F2B4D97FCCC33B90ED9'
+                    }
+                };
+                let modPath = req.body.OutputPath.replace("https://inforapp.immy.com","http://164.90.42.150:1435")
+
+                console.log(modPath)
+                fetch(modPath)
+                    .then(res => res.buffer())
+                    .then(buffer => {
+                        let reportFileName = "JobOperationListingReport.pdf"
+                        axios.post('https://immy2700.sharepoint.com/sites/production-automation/_api/web/GetFolderByServerRelativeURL' +
+                            '(\'/sites/production-automation/Shared Documents/Production Data/' + item + '/' + itemLotJob + '/\')/Files/add(url=\'' + reportFileName + '\',overwrite=true)'
+                            , buffer, config1)
+                            .then(function (resp) {
+                                //  console.log(resp.data);
+                            })
+                            .catch(function (error) {
+                                console.log(error.response.data);
+                            });
+                    })
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+})
+
 
 async function getAuthToken(itemLotJob, item, outputPath){
 
@@ -81,6 +135,10 @@ async function createFolder(authToken, itemLotJob, item, outputPath){
         }
     };
 
+    console.log(itemLotJob)
+    console.log(authToken)
+    console.log(item)
+
     axios.post('https://immy2700.sharepoint.com/sites/production-automation/_api/web/folders',{
             "__metadata": {
                 "type": "SP.Folder"
@@ -97,16 +155,7 @@ async function createFolder(authToken, itemLotJob, item, outputPath){
 }
 async function getFile1(itemLotJob, authToken, item, outputPath) {
 
-    let docID = "12899"
-    let config = {
-        headers: {
-            'Accept': 'application/json;odata=nometadata',
-            'Authorization': 'Bearer ' + authToken,
-            'X-RequestDigest': '0x703AE026F5E314B4F898AE91F77BDA1D192553AD50585C5018FCE8BD69BA6DC71EA26D88A4E650235663C76804D9048B2E7D1FB786702F2B4D97FCCC33B90ED9'
-        }
-    };
     console.log("Report: "+outputPath)
-
     sql.connect(conn).then(pool => {
         return pool.request()
             .query(`select DOCID from IMMYETQDocuments where item = '` + item + `'`)
@@ -117,18 +166,12 @@ async function getFile1(itemLotJob, authToken, item, outputPath) {
                 getFile2(itemLotJob, authToken, result.recordset[i].DOCID, item, outputPath)
             }
         }
-
-        //     let dataUpload = fs.readFileSync('C:\\Users\\sreenath-umagandhi\\Documents\\TestFolder\\'+itemLotJob+'\\'+itemLotJob+' '+fileName+'.xlsm', response.data)
-
     }).catch(function (error) {
-        console.log(error.response.data);
+        console.log(error);
     });
 }
 
 async function getFile2(itemLotJob, authToken, docID, item, outputPath) {
-
-  //  let docID = "9420"
-
     let config = {
         method: 'get',
         url: 'https://immy.etq.com/prod/rest/v1/documents/DOCWORK/DOCWORK_DOCUMENT/'+docID,
@@ -136,11 +179,8 @@ async function getFile2(itemLotJob, authToken, docID, item, outputPath) {
             'Authorization': 'Basic V1NfUHJvZF9JTU1ZOkcydmIzancx'
         }
     };
-
-
     axios(config)
         .then(function (response) {
-
             console.log(response.data.Document[0].Fields[16]);
             let x = response.data.Document[0].Fields[16].attachmentPath;
             console.log(response.data.Document[0].Fields[16].Values[0]);
@@ -157,9 +197,6 @@ async function getFile2(itemLotJob, authToken, docID, item, outputPath) {
                 },
                 responseType: 'arraybuffer'
             };
-
-
-
             axios(config1)
                 .then(function (response1) {
                     let config = {
@@ -170,30 +207,16 @@ async function getFile2(itemLotJob, authToken, docID, item, outputPath) {
                         }
                     };
                     console.log(response1.data)
-                    //     let dataUpload = fs.readFileSync('C:\\Users\\sreenath-umagandhi\\Documents\\TestFolder\\'+itemLotJob+'\\'+itemLotJob+' '+fileName+'.xlsm', response.data)
-    /*                axios.post('https://immy2700.sharepoint.com/sites/production-automation/_api/web/GetFolderByServerRelativeURL' +
+                    axios.post('https://immy2700.sharepoint.com/sites/production-automation/_api/web/GetFolderByServerRelativeURL' +
                         '(\'/sites/production-automation/Shared Documents/Production Data/'+item+'/'+itemLotJob+'/\')/Files/add(url=\''+itemLotJob+ ' '+fileName+'\',overwrite=true)'
                         ,response1.data,config)
                         .then(function (resp) {
-                            //  console.log(resp.data);
-                            fetch('https://inforapp.immy.com/SLReports/Report/OutputFiles/sreenath-umagandhi@immy.com/CCGJobOperationListing_DIMMY_264946.pdf')
-                                .then(res => res.buffer())
-                                .then(buffer => {
-                                    let reportFileName = "JobOperationListingReport.pdf"
-                                    axios.post('https://immy2700.sharepoint.com/sites/production-automation/_api/web/GetFolderByServerRelativeURL' +
-                                        '(\'/sites/production-automation/Shared Documents/Production Data/' + item + '/' + itemLotJob + '/\')/Files/add(url=\'' + reportFileName + '\',overwrite=true)'
-                                        , buffer, config)
-                                        .then(function (resp) {
-                                            //  console.log(resp.data);
-                                        })
-                                        .catch(function (error) {
-                                            console.log(error.response.data);
-                                        });
-                                })
+                              console.log("Created ETQ files successfully!");
+                             // reportUpload(outputPath, item, itemLotJob, config);
                         })
                         .catch(function (error) {
                             console.log(error.response.data);
-                        });*/
+                        });
                 })
 
         })
@@ -202,13 +225,33 @@ async function getFile2(itemLotJob, authToken, docID, item, outputPath) {
         });
 }
 
-function testingOn(){
+/*async function reportUpload(outputPath, item, itemLotJob, config) {
+    let modPath = outputPath.replace("https://inforapp.immy.com","http://164.90.42.150:1435")
+    console.log(modPath)
+    fetch(modPath)
+        .then(res => res.buffer())
+        .then(buffer => {
+            let reportFileName = "JobOperationListingReport.pdf"
+            axios.post('https://immy2700.sharepoint.com/sites/production-automation/_api/web/GetFolderByServerRelativeURL' +
+                '(\'/sites/production-automation/Shared Documents/Production Data/' + item + '/' + itemLotJob + '/\')/Files/add(url=\'' + reportFileName + '\',overwrite=true)'
+                , buffer, config)
+                .then(function (resp) {
+                    //  console.log(resp.data);
+                })
+                .catch(function (error) {
+                    console.log(error.response.data);
+                });
+        })
+}*/
+
+
+/*function testingOn(){
     axios.get('https://curlmyip.org')
         .then((response) => {
             console.log(response.data);
 
         });
-    fetch('https://164.90.42.150:1435/SLReports/Report/OutputFiles/sreenath-umagandhi@immy.com/CCGJobOperationListing_DIMMY_264946.pdf')
+    fetch('http://164.90.42.150:1435/SLReports/Report/OutputFiles/sreenath-umagandhi@immy.com/CCGJobOperationListing_DIMMY_264946.pdf')
         .then(res => res.buffer())
         .then(buffer => {
             console.log("success")
@@ -216,10 +259,8 @@ function testingOn(){
         }).catch(function (err){
             console.log(err);
     })
-}
+}*/
 
 app.listen(80, "0.0.0.0", ()=>console.log("Listening on port: " + 80));
-testingOn();
 https.createServer({} , app).listen(443, "0.0.0.0", ()=>console.log("Booted up https"));
-
 module.exports = app;
